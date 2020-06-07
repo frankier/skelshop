95f3426d880f8f77249bbff30911eaac62a8f3e2
Frankie Robertson <frankie@robertson.name>
2020-06-07 20:52:18 +0300

    Clean up install.sh script for style

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ shopt -s extglob
 git submodule update --init --recursive
 poetry install
 export SITE=$(echo $(poetry env list --full-path)/lib/python*/site-packages)
-echo "`pwd`/submodules/" > $SITE/lighttrack.pth
-echo "`pwd`/submodules/lighttrack" > $SITE/lighttrackinner.pth
-echo "`pwd`/submodules/lighttrack/graph" > $SITE/lighttrackgraph.pth
-echo "`pwd`/submodules/opencv_wrapper" > $SITE/opencv_wrapper.pth
-cd `pwd`/submodules/ufunclab && poetry run python setup.py install
+echo "$(pwd)/submodules/" > "$SITE/lighttrack.pth"
+echo "$(pwd)/submodules/lighttrack" > "$SITE/lighttrackinner.pth"
+echo "$(pwd)/submodules/lighttrack/graph" > "$SITE/lighttrackgraph.pth"
+echo "$(pwd)/submodules/opencv_wrapper" > "$SITE/opencv_wrapper.pth"
+cd $(pwd)/submodules/ufunclab && poetry run python setup.py install
