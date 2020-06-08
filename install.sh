ffa1ba2d4ce274b92de24e283be3686d3a539218
Frankie Robertson <frankie@robertson.name>
2020-06-08 07:30:03 +0300

    Make SITE in install.sh script fall backto global site-packages

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ shopt -s extglob
 git submodule update --init --recursive
 poetry install
 export SITE=$(echo $(poetry env list --full-path)/lib/python*/site-packages)
+if [ -z "$SITE" ]; then
+    export SITE="$(python -c 'import site; print(site.getsitepackages()[0])')"
+fi
 echo "$(pwd)/submodules/" > "$SITE/lighttrack.pth"
 echo "$(pwd)/submodules/lighttrack" > "$SITE/lighttrackinner.pth"
 echo "$(pwd)/submodules/lighttrack/graph" > "$SITE/lighttrackgraph.pth"
