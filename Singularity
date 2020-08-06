8c771e857efe79fa13161dcab0ee50be87a4115c
Frankie Robertson <frankie@robertson.name>
2020-08-06 15:23:05 +0300

    Make install script so can be installed on machine without NVidia GPU

--- Singularity
+++ Singularity
@@ -13,8 +13,6 @@ From: frankier/gsoc2020:frankier_gsoc2020
     poetry config virtualenvs.create false
     pip install --upgrade pip setuptools
     cd /opt/skelshop && \
-        FORCE_CUDA=1 \
-        TORCH_CUDA_ARCH_LIST='3.5;3.7;5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;3.5+PTX;3.7+PTX;5.0+PTX;5.2+PTX;5.3+PTX;6.0+PTX;6.1+PTX;6.2+PTX;7.0+PTX;7.2+PTX;7.5+PTX' \
         ./install_all.sh && \
         snakemake --cores 4
 
--- install.sh
+++ install.sh
@@ -24,4 +24,6 @@ echo "$(pwd)/submodules/lighttrack/graph/torchlight" > "$SITE/torchlight.pth"
 
 poetry run pip install -e $(pwd)/submodules/opencv_wrapper
 poetry run pip install -e $(pwd)/submodules/ufunclab
-FORCE_CUDA=1 poetry run pip install -e $(pwd)/submodules/mmskeleton
+FORCE_CUDA=1 \
+    TORCH_CUDA_ARCH_LIST='3.5;3.7;5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;3.5+PTX;3.7+PTX;5.0+PTX;5.2+PTX;5.3+PTX;6.0+PTX;6.1+PTX;6.2+PTX;7.0+PTX;7.2+PTX;7.5+PTX' \
+    poetry run pip install -e $(pwd)/submodules/mmskeleton
