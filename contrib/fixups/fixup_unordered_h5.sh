b382e659743ba7ff90e3f4173b9425ea0d02a2c1
Frankie Robertson <frankie@robertson.name>
2020-08-28 08:43:33 +0300

    Fixup fixup_unordered_h5.sh to use container paths

--- contrib/fixups/fixup_unordered_h5.sh
+++ contrib/fixups/fixup_unordered_h5.sh
@@ -3,7 +3,6 @@
 for h5file in work/pre-embed-hand-badorder/*.h5
 do
   srun -- singularity exec \
-    --bind `pwd`/fixup_unordered_h5.py \
     ~/sifs/skelshop_latest.sif bash -c \
-    "python `pwd`/fixup_unordered_h5.py $h5file work/pre-embed-hand/$(basename $h5file)" &
+    "python /opt/skelshop/contrib/fixups/fixup_unordered_h5.py $h5file work/pre-embed-hand/$(basename $h5file)" &
 done
