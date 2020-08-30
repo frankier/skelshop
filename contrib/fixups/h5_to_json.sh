5ea1aeb1094bd131f4c10f2c57f3581d1146fa7b
Frankie Robertson <frankie@robertson.name>
2020-08-30 07:09:56 +0300

    Fix h5_to_json.sh script

--- contrib/fixups/h5_to_json.sh
+++ contrib/fixups/h5_to_json.sh
@@ -4,7 +4,8 @@ mkdir -p work/pre-embed-hand-json
 
 for h5file in work/pre-embed-hand/*.h5
 do
+  h5bare=$(basename $h5file .h5)
   srun -- singularity exec \
     ~/sifs/skelshop_latest.sif bash -c \
-    "python /opt/skelshop/contrib/fixups/h5_to_json.py $h5file work/pre-embed-hand-json/$($(basename $h5file)%.h5).json" &
+    "python /opt/skelshop/contrib/fixups/h5_to_json.py $h5file > work/pre-embed-hand-json/$h5bare.json" &
 done
