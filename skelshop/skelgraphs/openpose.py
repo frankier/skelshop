9e36a87842c73e7ebd596b090da59f15cd37e208
Frankie Robertson <frankie@robertson.name>
2020-10-26 12:48:09 +0200

    Add BODY_25 to skelshop.skelgraphs.openpose exports

--- skelshop/skelgraphs/openpose.py
+++ skelshop/skelgraphs/openpose.py
@@ -1,10 +1,12 @@
 import os
 
-__all__ = ["MODE_SKELS", "BODY_ALL"]
+__all__ = ["MODE_SKELS", "BODY_ALL", "BODY_25"]
 
 if "LEGACY_SKELS" in os.environ:
+    from .openpose_legacy import BODY_25
     from .openpose_legacy import BODY_135 as BODY_ALL
     from .openpose_legacy import MODE_SKELS
 else:
+    from .openpose_multi import BODY_25
     from .openpose_multi import BODY_25_ALL as BODY_ALL
     from .openpose_multi import MODE_SKELS
