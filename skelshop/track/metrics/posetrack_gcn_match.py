016ab42a725fda61933d89703f449ebe9fd6d5c8
Frankie Robertson <frankie@robertson.name>
2020-11-03 16:04:04 +0200

    Make PoseMatcher lazy loaded & increase min Python version to 3.7

--- skelshop/track/metrics/posetrack_gcn_match.py
+++ skelshop/track/metrics/posetrack_gcn_match.py
@@ -1,40 +1,55 @@
-import argparse
+from typing import Any
 
-import torch
-from lighttrack.graph.gcn_utils.io import IO
-from lighttrack.graph.gcn_utils.processor_siamese_gcn import SGCN_Processor
+PoseMatcher: Any
 
 
-class PoseMatcher(SGCN_Processor):
-    def __init__(self, config):
-        self.config = config
-        argv = []
-        if not torch.cuda.is_available():
-            argv.extend(["--use_gpu", "0"])
-        self.load_arg(argv)
-        self.init_environment()
-        self.load_model()
-        self.load_weights()
-        self.gpu()
-        return
+def _mk_pose_matcher():
+    import argparse
 
-    def get_parser(self, add_help=False):
-        parent_parser = IO.get_parser(add_help=False)
-        parser = argparse.ArgumentParser(
-            add_help=False,
-            parents=[parent_parser],
-            description="Graph Convolution Network for Pose Matching",
-        )
+    import torch
+    from lighttrack.graph.gcn_utils.io import IO
+    from lighttrack.graph.gcn_utils.processor_siamese_gcn import SGCN_Processor
 
-        parser.set_defaults(config=self.config)
-        return parser
+    class PoseMatcher(SGCN_Processor):
+        def __init__(self, config):
+            self.config = config
+            argv = []
+            if not torch.cuda.is_available():
+                argv.extend(["--use_gpu", "0"])
+            self.load_arg(argv)
+            self.init_environment()
+            self.load_model()
+            self.load_weights()
+            self.gpu()
+            return
 
-    def preproc(self, data):
-        self.model.eval()
+        def get_parser(self, add_help=False):
+            parent_parser = IO.get_parser(add_help=False)
+            parser = argparse.ArgumentParser(
+                add_help=False,
+                parents=[parent_parser],
+                description="Graph Convolution Network for Pose Matching",
+            )
 
-        with torch.no_grad():
-            data = torch.from_numpy(data)
-            data = data.unsqueeze(0)
-            data = data.float().to(self.dev)
-            data = self.model.extract_feature(data)
-            return data.cpu().numpy()[0]
+            parser.set_defaults(config=self.config)
+            return parser
+
+        def preproc(self, data):
+            self.model.eval()
+
+            with torch.no_grad():
+                data = torch.from_numpy(data)
+                data = data.unsqueeze(0)
+                data = data.float().to(self.dev)
+                data = self.model.extract_feature(data)
+                return data.cpu().numpy()[0]
+
+    return PoseMatcher
+
+
+def __getattr__(name: str) -> Any:
+    if name == "PoseMatcher":
+        global PoseMatcher
+        PoseMatcher = _mk_pose_matcher()
+        return PoseMatcher
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
