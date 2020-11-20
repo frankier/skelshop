d40dca007d40c72992de9631c73ad6ac7d141cf2
Chris Stenkamp <cstenkamp@gmail.com>
2020-11-20 16:09:58 +0100

    deps

--- skelshop/test/test_errorfree_run.py
+++ skelshop/test/test_errorfree_run.py
@@ -1,13 +1,13 @@
+import os
 import tempfile
 from os.path import join
-import os
 
 import requests
 from pytest import fixture
 
 import skelshop.config as config
-from skelshop.test.testing_conf import TestingConf as testconf
 from skelshop.cmd.playsticks import playsticks_fn
+from skelshop.test.testing_conf import TestingConf as testconf
 
 
 @fixture
@@ -20,27 +20,21 @@ def conf():
 def test_data(conf):
     res = {}
     tmpdir = tempfile.mkdtemp()
-    download_names = {'vid_file': 'shorter_ellen.avi', 'skel_file': 'shorter_ellen_skel.h5'}
+    download_names = {
+        "vid_file": "shorter_ellen.avi",
+        "skel_file": "shorter_ellen_skel.h5",
+    }
     for key, file in download_names.items():
-        resp = requests.get(conf.TESTFILES_URL+'/'+file, allow_redirects=True)
-        with open(join(tmpdir, file), 'wb') as ofile:
+        resp = requests.get(conf.TESTFILES_URL + "/" + file, allow_redirects=True)
+        with open(join(tmpdir, file), "wb") as ofile:
             ofile.write(resp.content)
         res[key] = join(tmpdir, file)
     return res
 
 
 def test_errorfree_run_drawsticks_cmd(conf, test_data):
-    '''cd /home/chris/Documents/JOBS/Uhrig-Gesture-Recog/skelshop && SDL_VIDEODRIVER=dummy;PYTHONUNBUFFERED=1; \
-    OPENPOSE=/home/chris/Documents/JOBS/Uhrig-Gesture-Recog/skelshop_deps/openpose_cpu/; \
-    LD_LIBRARY_PATH=/home/chris/Documents/JOBS/Uhrig-Gesture-Recog/skelshop_deps/openpose_cpu/build/src/openpose/; \
-    PYTHONPATH=/home/chris/Documents/JOBS/Uhrig-Gesture-Recog/skelshop_deps/openpose_cpu/build/python/; \
-    MODEL_FOLDER=/home/chris/Documents/JOBS/Uhrig-Gesture-Recog/skelshop_deps/openpose_cpu/models  \
-    poetry run python skelshop --ffprobe-bin /usr/bin/ffprobe playsticks \
-    /home/chris/Documents/JOBS/Uhrig-Gesture-Recog/data/short_ellen.avi --skel \
-    /home/chris/Documents/JOBS/Uhrig-Gesture-Recog/data/short_ellen_bkp.h5'''
     os.environ["SDL_VIDEODRIVER"] = "dummy"
-    playsticks_fn(test_data['vid_file'], (test_data['skel_file'], ))
-
+    playsticks_fn(test_data["vid_file"], (test_data["skel_file"],))
 
 
 def main():
@@ -49,6 +43,5 @@ def main():
     test_errorfree_run_drawsticks_cmd(conf_, test_data_)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
